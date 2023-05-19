cf70f665e102cc07ff5a6bcff9a1e066a28abb98
Luca Mastrodicasa <108471545+palucaville@users.noreply.github.com>
2023-05-19 23:30:45 +0100

    Update fetchBTC.py
    better formatting

--- fetchBTC.py
+++ fetchBTC.py
@@ -1,6 +1,16 @@
+bl_info = {
+    # required
+    'name': 'fetchBTC',
+    'blender': (2, 80, 0),
+    'category': 'Object',
+    # optional
+    'version': (1, 0, 0),
+    'author': 'Luca Mastrodicasa github.com/palucaville/',
+    'description': 'Fetch current bitcoin price in US dollars',
+}
 
 import bpy
-import requests
+import requests # this is needed and usually NOT included in standard python
 import time
 from datetime import datetime
 
@@ -21,82 +31,40 @@ def draw_BTC(k):
       # -- Set scale and location
     obj.location = (0, k, 0.1) # move on the Z axis
     obj.scale = (0.75, 0.5, 0.5)
-    
-    mat = newShader("Shader2", "diffuse", 0.1, 0.1, 0.1)
-    obj.data.materials.append(mat)
     bpy.context.scene.collection.objects.link(obj)  
 
 def draw_dt(k):
     
-     # Getting the current date and time
+# Getting the current date and time
     datt = datetime.now()
     dt =str(datt)
-    print("Date and time is:", dt)
+    #print("Date and time is:", dt) #debug print
     font_curve2 = bpy.data.curves.new(type="FONT", name="dt")
     font_curve2.body = dt 
     obj2 = bpy.data.objects.new(name="Font Object2", object_data=font_curve2)
-      
-      # -- Set scale and location
+  
+# -- Set scale and location
     obj2.location = (0, k+0.5, 0.1) 
     obj2.scale = (0.5, 0.3, 0.3)
-    
     bpy.context.scene.collection.objects.link(obj2)
-    
-def newMaterial(id):
 
-    mat = bpy.data.materials.get(id)
-    if mat is None:
-        mat = bpy.data.materials.new(name=id)
-    mat.use_nodes = True
-    if mat.node_tree:
-        mat.node_tree.links.clear()
-        mat.node_tree.nodes.clear()
-    return mat
+counter = 5 # how many times to fetch
 
-def newShader(id, type, r, g, b):
-
-    mat = newMaterial(id)
-    nodes = mat.node_tree.nodes
-    links = mat.node_tree.links
-    output = nodes.new(type='ShaderNodeOutputMaterial')
-    if type == "diffuse":
-        shader = nodes.new(type='ShaderNodeBsdfDiffuse')
-        nodes["Diffuse BSDF"].inputs[0].default_value = (r, g, b, 1)
-    elif type == "emission":
-        shader = nodes.new(type='ShaderNodeEmission')
-        nodes["Emission"].inputs[0].default_value = (r, g, b, 1)
-        nodes["Emission"].inputs[1].default_value = 1
-    elif type == "glossy":
-        shader = nodes.new(type='ShaderNodeBsdfGlossy')
-        nodes["Glossy BSDF"].inputs[0].default_value = (r, g, b, 1)
-        nodes["Glossy BSDF"].inputs[1].default_value = 0
-
-    links.new(shader.outputs[0], output.inputs[0])
-
-    return mat
-
-#bpy.ops.object.delete(use_global=False)
-
-#for k in range(4):
-
-#k = 0   
-#draw_BTC(k)
-#draw_dt(k)
-
-
-counter = 0
-
-def run_10_times():
+def run_n_times():
     global counter
-    counter += 1
+    counter -= 1
     draw_BTC(counter)
     draw_dt(counter)
     
-    if counter == 10:
+    if counter == 0:
         return None
-    return 10 #seconds
+    return 17 # fetch every this many seconds
 
-bpy.app.timers.register(run_10_times)
+bpy.app.timers.register(run_n_times)
+bpy.ops.object.select_all(action='SELECT')
+bpy.ops.transform.translate(value=(7.1068, 0, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+bpy.ops.object.select_all(action='DESELECT')
+
 
 
 
