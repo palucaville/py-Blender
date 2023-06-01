336975f9efd19517897243ba8526da948f6d8432
Luca Mastrodicasa <108471545+palucaville@users.noreply.github.com>
2023-06-01 14:20:39 +0100

    Update fetchBTC.py
    added random colour

--- fetchBTC.py
+++ fetchBTC.py
@@ -9,10 +9,23 @@ bl_info = {
     'description': 'Fetch current bitcoin price in US dollars',
 }
 
-import bpy
-import requests # this is needed and usually NOT included in standard python
+import bpy# this is needed and usually NOT included in standard python
+from mathutils import Color
 import time
+import random
 from datetime import datetime
+
+
+counter = 5 # CHANGE THIS - ow many times to fetch BTC price
+
+def run_n_times():
+    global counter
+    counter -= 1
+    #draw_BTC(counter)
+    draw_dt(counter)
+    if counter == 0:
+        return None
+    return 5 #  CHANGE THIS - fetch every THIS many seconds
 
 def get_btc_current():
     try:
@@ -35,34 +48,66 @@ def draw_BTC(k):
 
 def draw_dt(k):
     
-# Getting the current date and time
-    datt = datetime.now()
+    # Getting the current date and time
+    datt = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
     dt =str(datt)
     #print("Date and time is:", dt) #debug print
     font_curve2 = bpy.data.curves.new(type="FONT", name="dt")
     font_curve2.body = dt 
     obj2 = bpy.data.objects.new(name="Font Object2", object_data=font_curve2)
   
-# -- Set scale and location
-    obj2.location = (0, k+0.5, 0.1) 
+    # -- Set scale and location
+    obj2.location = (0, k+0.5, 0.1) # change to a minus to go down
     obj2.scale = (0.5, 0.3, 0.3)
     bpy.context.scene.collection.objects.link(obj2)
 
-counter = 5 # how many times to fetch
 
-def run_n_times():
-    global counter
-    counter -= 1
-    draw_BTC(counter)
-    draw_dt(counter)
+
+def create_random_material():
     
-    if counter == 0:
-        return None
-    return 17 # fetch every this many seconds
+    # Create a new material
+    material = bpy.data.materials.new(name="Random Material")
+    material.use_nodes = True
+    nodes = material.node_tree.nodes
+    # Clear default nodes
+    for node in nodes:
+        nodes.remove(node)
+    #  diffuse shader node
+    diffuse = nodes.new(type='ShaderNodeBsdfDiffuse')
+    diffuse.location = (0, 0)
+    output = nodes.new(type='ShaderNodeOutputMaterial')
+    output.location = (200, 0)
+    links = material.node_tree.links
+    link = links.new(diffuse.outputs['BSDF'], output.inputs['Surface'])
+    # random color for the diffuse node
+    diffuse.inputs['Color'].default_value = [random.random() for _ in range(3)] + [1]  # RGBA format
+
+    return material
+
+
+# Get the active scene
+scene = bpy.context.scene
 
 bpy.app.timers.register(run_n_times)
-bpy.ops.object.select_all(action='SELECT')
-bpy.ops.transform.translate(value=(7.1068, 0, 0), orient_axis_ortho='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+
+bpy.ops.object.select_all(action='SELECT') 
+# Create a random material
+random_material = create_random_material()
+selected_objects = bpy.context.selected_objects
+# Assign the random material to all selected objects
+for obj in selected_objects:
+    if obj.type == 'FONT':
+        if obj.data.materials:
+            # If the object already has materials, replace the first one with the random material
+            obj.data.materials[0] = random_material
+        else:
+            # If the object doesn't have any materials, create a new one and assign it
+            obj.data.materials.append(random_material)
+bpy.ops.transform.translate(value=(0, -1, -5))
+
+# Update the view
+bpy.context.view_layer.update()
+
 bpy.ops.object.select_all(action='DESELECT')
 
 
